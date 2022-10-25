368426ccdc50d6523b134176fb71203bef603411
unknown <quinchej659@gmail.com>
2022-10-25 09:12:51 -0500

    chiquitrosfs

---
+++ src/prueba.java
@@ -0,0 +1,7 @@
+public class prueba {
+    public static void main(String[] args) {
+        System.out.println("generacion del proyecto");
+        System.out.println(" para git como ejemplo");
+
+    }
+}
